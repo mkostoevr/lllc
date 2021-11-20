4397cadd69e28b1dfe50e590c02374c32d5cd2f1
mkostoevr <mkostoevr@yandex.ru>
2021-11-21 02:11:17 +0300

    [TEST] Update test system output

--- runtests.py
+++ runtests.py
@@ -4,12 +4,17 @@ import os
 import sys
 from importlib.machinery import SourceFileLoader
 from multiprocessing.pool import ThreadPool
+from threading import Lock
 import traceback
 import timeit
 
+tests = []
 failed_tests = []
+finished_tests = [] # Both succeed and failed
+callback_lock = Lock()
 test_start_times = {}
 root_dir = os.getcwd()
+test_time = 0.0
 
 def collect_tests(path):
     tests = []
@@ -27,6 +32,17 @@ def collect_tests(path):
             tests += collect_tests(test_folder_path)
     return tests
 
+def get_progress():
+    return f"[{len(finished_tests) + 1}/{len(tests)}]"
+
+def get_status_from_exception(ex):
+    if type(ex) == common.TestFailureException:
+        status = "FAILURE"
+    elif type(ex) == common.TestTimeoutException:
+        status = "TIMEOUT"
+    else:
+        status = "UNKNOWN"
+
 def get_test_file_from_exception(ex):
     tb = traceback.extract_tb(ex.__traceback__)
     for frame in tb:
@@ -43,21 +59,26 @@ def run_test(root_dir, test):
     return test
 
 def callback(arg):
-    test = arg
-    time = timeit.default_timer() - test_start_times[test]
-    print(f"{test}: SUCCESS ({time:.2f} seconds)\n", end = "", flush = True)
+    global test_time
+    with callback_lock:
+        test = arg
+        time = timeit.default_timer() - test_start_times[test]
+        test_time += time
+        progress = get_progress()
+        print(f"{progress} {test}: SUCCESS ({time:.2f} seconds)", flush = True)
+        finished_tests.append(test)
 
 def error_callback(ex):
-    if type(ex) == common.TestFailureException:
-        status = "FAILURE"
-    elif type(ex) == common.TestTimeoutException:
-        status = "TIMEOUT"
-    else:
-        status = "UNKNOWN"
-    test = get_test_file_from_exception(ex)
-    time = timeit.default_timer() - test_start_times[test]
-    print(f"{test}: {status} ({time:.2f} seconds)\n", end = "", flush = True)
-    failed_tests.append(test)
+    global test_time
+    with callback_lock:
+        status = get_status_from_exception(ex)
+        test = get_test_file_from_exception(ex)
+        time = timeit.default_timer() - test_start_times[test]
+        test_time += time
+        progress = get_progress()
+        print(f"{progress} {test}: {status} ({time:.2f} seconds)", flush = True)
+        finished_tests.append(test)
+        failed_tests.append(test)
 
 if __name__ == '__main__':
     sys.path.append('test')
@@ -65,9 +86,8 @@ if __name__ == '__main__':
 
     tests = collect_tests('test')
 
-    test_executors = ThreadPool()
-
     # Execute each test
+    test_executors = ThreadPool()
     test_start = timeit.default_timer()
     for test in tests:
         test_executors.apply_async(run_test,
@@ -77,9 +97,11 @@ if __name__ == '__main__':
     test_executors.close()
     test_executors.join()
     test_end = timeit.default_timer()
+    threading_speedup = f"{test_time / (test_end - test_start) * 100:.2f}%"
     print(f"Overall time: {test_end - test_start:.2f} seconds")
+    print(f"Summarized test time: {test_time:.2f} ({threading_speedup})")
 
     if len(failed_tests) != 0:
         print(f"Failed: {len(failed_tests)}/{len(tests)}")
         for failed_test in failed_tests:
-            print(f"{failed_test}")
+            print(f"  {failed_test}")
