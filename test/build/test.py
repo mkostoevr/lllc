8fe5ac9d31711f7bb99d19e46f26de68a9f98b8d
mkostoevr <mkostoevr@yandex.ru>
2021-11-22 22:16:35 +0300

    [TEST] Change test binary output path

--- test/build/test.py
+++ test/build/test.py
@@ -6,7 +6,9 @@ sys.path.append('../')
 import common
 
 def run(project_root, test_root):
-	if os.system(f"tcc {project_root}/*.c -o lllc.exe") != 0:
+	lllc0_exe = f"{test_root}/lllc0.exe"
+	lllc1_exe = f"{test_root}/lllc1.exe"
+	if os.system(f"tcc {project_root}/*.c -o {lllc0_exe}") != 0:
 		raise common.TestFailureException()
-	if os.system(f"tcc {project_root}/main.c -DONE_SOURCE -o lllc.exe") != 0:
+	if os.system(f"tcc {project_root}/main.c -DONE_SOURCE -o {lllc1_exe}") != 0:
 		raise common.TestFailureException()
